c2976da9f3368ae1f49c4208331537243564d10f
Landscape <494326656@qq.com>
2017-03-31 15:22:19 +0800

    modify gradle conf way & add res which lost

--- app/src/main/java/com/landscape/dragcalendar/constant/Range.java
+++ app/src/main/java/com/landscape/dragcalendar/constant/Range.java
@@ -7,4 +7,6 @@ public class Range {
     public static final int MONTH_HEIGHT = 270;
     public static final int WEEK_HEIGHT = 45;
     public static final int DAY_HEIGHT = 45;
+    public static final int MONTH_PADDING_BOTTOM = 20;
+    public static final int MONTH_PADDING_TOP = 20;
 }
